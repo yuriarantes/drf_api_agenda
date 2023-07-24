e8bb0fa3e9d90444b0e591881744b35798223eaf
Yuri Arantes <falaryuriarantes@gmail.com>
2023-07-24 19:49:07 -0300

    Refactoring the project, i'm use just one models for this simple project

--- niry_agenda/core/models.py
+++ niry_agenda/core/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Scheduling(models.Model):
+    scheduling_date = models.DateTimeField()
+    name = models.CharField(max_length=240)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
