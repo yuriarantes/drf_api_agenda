90a693c7f09c5e8e1393fe0b236c153536de06df
Yuri Arantes <falaryuriarantes@gmail.com>
2023-07-26 08:58:56 -0300

    Add methods create and update to Scheduling

--- niry_agenda/core/v1/serializers.py
+++ niry_agenda/core/v1/serializers.py
@@ -9,3 +9,23 @@ class SchedulingSerializer(serializers.Serializer):
     phone = serializers.CharField(max_length=15)
     active = serializers.BooleanField(allow_null=True)
     
+    def create(self, validated_data):
+        scheduling = Scheduling.objects.create(
+            scheduling_date = validated_data['scheduling_date'],
+            name = validated_data['name'],
+            email = validated_data['email'],
+            phone = validated_data['phone'],
+            active = validated_data['active'],
+        )
+
+        return scheduling
+    
+    def update(self, instance, validated_data):
+        instance.scheduling_date = validated_data.get('scheduling_date', instance.scheduling_date)
+        instance.name = validated_data.get('name', instance.name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.phone = validated_data.get('phone', instance.phone)
+        instance.active = validated_data.get('active', instance.active)
+        instance.save()
+
+        return instance
