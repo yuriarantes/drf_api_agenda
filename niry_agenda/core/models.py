f508f19c181c1e8caec44f2482cfd7d4c605b9d2
Yuri Arantes <falaryuriarantes@gmail.com>
2023-07-25 23:08:03 -0300

    Add unique True in 'name' field

--- niry_agenda/core/models.py
+++ niry_agenda/core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Scheduling(models.Model):
     scheduling_date = models.DateTimeField()
-    name = models.CharField(max_length=240)
+    name = models.CharField(max_length=240, unique=True)
     email = models.EmailField()
     phone = models.CharField(max_length=15)
     active = models.BooleanField(null=True)
