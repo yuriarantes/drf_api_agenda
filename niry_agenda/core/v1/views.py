48dbc0d52d7810723831ebbbf989edc9247be2b2
Yuri Arantes <falaryuriarantes@gmail.com>
2023-07-26 09:00:05 -0300

    Add new methods, delete, patch and refactor .save()

--- niry_agenda/core/v1/views.py
+++ niry_agenda/core/v1/views.py
@@ -1,11 +1,12 @@
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from ..models import Scheduling
 from .serializers import SchedulingSerializer
 
-@api_view(http_method_names=['GET','PUT','PATCH'])
+@api_view(http_method_names=['GET','PATCH','DELETE'])
 def scheduling_detail(request, id):
     if request.method == "GET":
         """
@@ -17,37 +18,38 @@ def scheduling_detail(request, id):
 
         return JsonResponse(serializer.data)
 
-    if request.method == "PUT":
+    if request.method == "PATCH":
+        """
+        Update partial from schdulings for id and return details
+        """
         obj = get_object_or_404(Scheduling, id=id)
 
-        serializer = SchedulingSerializer(data=request.data)
+        serializer = SchedulingSerializer(obj,data=request.data, partial=True)
 
         if serializer.is_valid():
-            v_data = serializer.validated_data
+            serializer.save()
 
-            obj.scheduling_date = v_data.get('scheduling_date', obj.scheduling_date)
-            obj.name = v_data.get('name', obj.name)
-            obj.email = v_data.get('email', obj.email)
-            obj.phone = v_data.get('phone', obj.phone)
-            obj.active = v_data.get('active', obj.active)
-
-            obj.save()
-
-            new_serializer = SchedulingSerializer(get_object_or_404(Scheduling, id=id))
-
-            new_data = new_serializer.data
-
-            return JsonResponse(new_data, status=204, safe=False)
+            return JsonResponse(serializer.data, status=200, safe=False)
         
         return JsonResponse(serializer.errors, status=400)
+    if request.method == "DELETE":
+        """
+        Remove schdulings for id
+        """
+        obj = get_object_or_404(Scheduling,id=id)
+        obj.active = False
+        obj.save()
+
+        return   Response(status=204)
+
 
 @api_view(http_method_names=['GET','POST'])
 def scheduling_list(request):
     if request.method == "GET":
         """
-        Search schdulings, return list
+        Search schdulings, return list of scheduling actives
         """
-        qs = Scheduling.objects.all()
+        qs = Scheduling.objects.filter(active=True)
 
         serializer = SchedulingSerializer(qs, many=True)
 
@@ -61,15 +63,7 @@ def scheduling_list(request):
         serializer = SchedulingSerializer(data=data)
 
         if serializer.is_valid():
-            validated_data = serializer.validated_data
-
-            Scheduling.objects.create(
-                scheduling_date = validated_data['scheduling_date'],
-                name = validated_data['name'],
-                email = validated_data['email'],
-                phone = validated_data['phone'],
-                active = validated_data['active'],
-            )
+            serializer.save()
 
             return JsonResponse(serializer.data, status=201)
 
