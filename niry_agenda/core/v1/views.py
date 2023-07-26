8c2d424d38de3a883b7fc6cc204b43f6b989148d
Yuri Arantes <falaryuriarantes@gmail.com>
2023-07-25 21:33:37 -0300

    Configuration from schedulin_detail, to return data from scheduling

--- niry_agenda/core/v1/views.py
+++ niry_agenda/core/v1/views.py
@@ -2,14 +2,22 @@ from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 
-from core.models import Scheduling
+from ..models import Scheduling
+from .serializers import SchedulingSerializer
 
 @api_view(http_method_names=['GET'])
 def scheduling_detail(request, id):
     """
     Search schdulings for id and return details
     """
-    scheduling = get_object_or_404(Scheduling, id=id)
+    obj = get_object_or_404(Scheduling, id=id)
+
+    serializer = SchedulingSerializer(obj)
+
+    if serializer.is_valid():
+        return JsonResponse(serializer.data)
+    else:
+        return JsonResponse(serializer.errors)
 
 
 @api_view(http_method_names=['GET'])
