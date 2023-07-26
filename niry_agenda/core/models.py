c60595ac57d361a2a3927b8eb9d99768f525b776
Yuri Arantes <falaryuriarantes@gmail.com>
2023-07-25 21:32:20 -0300

    Add new field (active)

--- niry_agenda/core/models.py
+++ niry_agenda/core/models.py
@@ -5,3 +5,4 @@ class Scheduling(models.Model):
     name = models.CharField(max_length=240)
     email = models.EmailField()
     phone = models.CharField(max_length=15)
+    active = models.BooleanField(null=True)
