7bcfdb5e23afe53eafa4e65bbd46b1281583d8bf
Yuri Arantes <falaryuriarantes@gmail.com>
2023-07-25 21:34:52 -0300

    Update serializer to the new models

--- niry_agenda/core/v1/serializers.py
+++ niry_agenda/core/v1/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
-from models import Scheduling
+from ..models import Scheduling
 
 class SchedulingSerializer(serializers.Serializer):
-    model = Scheduling
-    fields = ['scheduling_date','name','email','phone']
-
+    scheduling_date = serializers.DateTimeField()
+    name = serializers.CharField(max_length=240)
+    email = serializers.EmailField()
+    phone = serializers.CharField(max_length=15)
+    active = serializers.BooleanField(allow_null=True)
     
