2222dec722d76e6179b337c2bfdde2d2206d71a2
Yuri Arantes <falaryuriarantes@gmail.com>
2023-07-25 23:10:20 -0300

    Replacing scheduling endpoint from ../schedulings to ../scheduling

--- niry_agenda/core/v1/urls.py
+++ niry_agenda/core/v1/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import scheduling_detail, scheduling_list
 
 urlpatterns_schedulings=[
-    path('v1/schedulings/', scheduling_list),
+    path('v1/scheduling/', scheduling_list),
     path('v1/scheduling/<int:id>/',scheduling_detail),
 ]
