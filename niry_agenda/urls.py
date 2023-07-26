16bad9b38839e04b2f6c22daed6d4a7906c208ff
Yuri Arantes <falaryuriarantes@gmail.com>
2023-07-25 21:31:03 -0300

    Add endpoints from schedulings

--- niry_agenda/urls.py
+++ niry_agenda/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from .core.v1.urls import urlpatterns_schedulings as urls_schedulling
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(urls_schedulling))
 ]
