b4308e3013a76f2c3f3ebba6f7e7f9206ed61522
Yuri Arantes <falaryuriarantes@gmail.com>
2023-07-31 08:59:39 -0300

    Include config to admin, field, search, etc;

--- niry_agenda/core/admin.py
+++ niry_agenda/core/admin.py
@@ -3,22 +3,32 @@ from django.contrib import admin
 from .models import Scheduling, Schedule, Store
 #from .actions import make_cancelled
 
-def scheduling_cancel(modeladmin, request, queryset):
-    queryset.update(active=False)
+class StoreAdmin(admin.ModelAdmin):
+    list_display=['id','social_name','cnpj','active']
+    search_fields=['social_name','cnpj']
 
-def scheduling_activate(modeladmin, request, queryset):
-    queryset.update(active=True)
-
-scheduling_cancel.short_description = "Cancelar agendamento"
-scheduling_activate.short_description = "Ativar agendamento"
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display=['id','store','day','first_start_at','first_end_at','last_start_at','last_end_at']
+    search_fields=['store','day']
+    list_filter=['store']
 
 class SchedulingAdmin(admin.ModelAdmin):
     list_display = ['id','scheduling_date', 'name', 'email', 'phone','active']
     list_filter = ['scheduling_date']
     search_fields = ['scheduling_date','name']
     list_editable = ['name']
+    
+    def scheduling_cancel(modeladmin, request, queryset):
+        queryset.update(active=False)
+
+    def scheduling_activate(modeladmin, request, queryset):
+        queryset.update(active=True)
+
+    scheduling_cancel.short_description = "Cancelar agendamento"
+    scheduling_activate.short_description = "Ativar agendamento"
+
     actions = [scheduling_cancel,scheduling_activate]
 
 admin.site.register(Scheduling, SchedulingAdmin)
-admin.site.register(Store)
-admin.site.register(Schedule)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(Schedule, ScheduleAdmin)
