e6c57a4725d1d0a4dd85dbe0e6b933e63b33ea98
Yuri Arantes <falaryuriarantes@gmail.com>
2023-07-31 09:00:20 -0300

    Altering models from Store and Schedule

--- niry_agenda/core/models.py
+++ niry_agenda/core/models.py
@@ -3,6 +3,10 @@ from django.db import models
 class Store(models.Model):
     social_name = models.CharField(max_length=240, unique=True)
     cnpj = models.CharField(max_length=14)
+    active = models.BooleanField(null=True)
+
+    def __str__(self) -> str:
+        return self.social_name
 
 class Schedule(models.Model):
     DAY_CHOICES = (
@@ -15,12 +19,12 @@ class Schedule(models.Model):
         ('6','Sunday'),
     )
 
-    store_id = models.ForeignKey(Store, on_delete=models.CASCADE)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
     day = models.CharField(max_length=20, choices=DAY_CHOICES)
     first_start_at = models.TimeField()
     first_end_at = models.TimeField()
-    last_start_at = models.TimeField()
-    last_end_at = models.TimeField()
+    last_start_at = models.TimeField(null=True, blank=True)
+    last_end_at = models.TimeField(null=True, blank=True)
 
 class Scheduling(models.Model):
     scheduling_date = models.DateTimeField()
@@ -28,4 +32,9 @@ class Scheduling(models.Model):
     email = models.EmailField()
     phone = models.CharField(max_length=15)
     active = models.BooleanField(null=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 
