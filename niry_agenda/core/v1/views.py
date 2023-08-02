6a10f9c6406321477d3703a2c57f02d16601a5e6
Yuri Arantes <falaryuriarantes@gmail.com>
2023-08-01 23:57:46 -0300

    Adding new view to schedule, the schedule_list, this view return all schedules available

--- niry_agenda/core/v1/views.py
+++ niry_agenda/core/v1/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from ..models import Scheduling
+from ..models import Scheduling, Schedule, Store
 from .serializers import SchedulingSerializer
+
+from datetime import datetime, timedelta
 
 @api_view(http_method_names=['GET','PATCH','DELETE'])
 def scheduling_detail(request, id):
@@ -67,4 +69,43 @@ def scheduling_list(request):
             return JsonResponse(serializer.data, status=201)
 
         return JsonResponse(serializer.errors, status=400)
+
+@api_view(http_method_names=['GET'])
+def schedule_list(request):
+    schedules = []
+
+    store_id = request.query_params.get('store')
+    date = datetime.strptime(request.query_params.get('date'),'%Y-%m-%d').date()
+
+    obj_schedule = Schedule.objects.filter(store=store_id,day=date.weekday()).first()
+
+    time = obj_schedule.first_start_at
+    first_end_at = obj_schedule.first_end_at
+
+    if obj_schedule.last_end_at:
+        last_time = obj_schedule.last_end_at
+    else:
+        last_time = obj_schedule.first_end_at
+
+    while time < last_time:
+        if obj_schedule.last_start_at:
+            if time >= obj_schedule.first_end_at and time < obj_schedule.last_start_at:
+                ...
+        schedules.append(time)
+
+    
+        time = (datetime.combine(datetime.today(), time) + timedelta(minutes=30)).time()
+        
+
+    print(obj_schedule.first_start_at)
+    print(obj_schedule.last_start_at)
+
+
+    dict = {
+        "store": store_id,
+        "date": date,
+        "times":schedules,
+    }
+
+    return JsonResponse(dict)
 
