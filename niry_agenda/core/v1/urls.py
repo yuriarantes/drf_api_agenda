476724a1cc1fdf7d13b6726e8063c3145a4c1f0f
Yuri Arantes <falaryuriarantes@gmail.com>
2023-08-01 23:49:30 -0300

    Adding new endpoints to Schedules

--- niry_agenda/core/v1/urls.py
+++ niry_agenda/core/v1/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from .views import scheduling_detail, scheduling_list
+from .views import scheduling_detail, scheduling_list, schedule_list
 
 urlpatterns_schedulings=[
     path('v1/scheduling/', scheduling_list),
     path('v1/scheduling/<int:id>/',scheduling_detail),
+]
+
+urlpatterns_schedules=[
+    path('v1/schedule/',schedule_list)
 ]
