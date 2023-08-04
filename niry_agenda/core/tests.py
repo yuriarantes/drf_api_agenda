682c94e9ed293b99bb4d092607b4df785ba9ed3c
Yuri Arantes <falaryuriarantes@gmail.com>
2023-08-03 23:55:37 -0300

    Create tests to API's

--- niry_agenda/core/tests.py
+++ niry_agenda/core/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+import json
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class TestSchedulingList(APITestCase):
+    def test_listagem_vazia(self):
+        response = self.client.get("/api/v1/scheduling/")
+        data = json.loads(response.content)
+        
+        self.assertEqual(data,[])
