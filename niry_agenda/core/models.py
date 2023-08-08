c3034852cd29220e4d705d95a6b26aacb8463630
Yuri Arantes <falaryuriarantes@gmail.com>
2023-08-07 23:32:05 -0300

    Adding new model, model client, this model storage client infos

--- niry_agenda/core/models.py
+++ niry_agenda/core/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+class Client(models.Model):
+    name = models.CharField(max_length=240, unique=True)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    active = models.BooleanField(null=False, default=True)
+
 class Store(models.Model):
     social_name = models.CharField(max_length=240)
     cnpj = models.CharField(max_length=14, unique=True)
@@ -29,10 +35,7 @@ class Schedule(models.Model):
 class Scheduling(models.Model):
     scheduling_date = models.DateTimeField()
     store = models.ForeignKey(Store, on_delete=models.CASCADE, null=False)
-    name = models.CharField(max_length=240, unique=True)
-    email = models.EmailField()
-    phone = models.CharField(max_length=15)
-    active = models.BooleanField(null=False, default=True)
+    client  = models.ForeignKey(Client, on_delete=models.CASCADE,null=False)
     
 
     def __str__(self) -> str:
