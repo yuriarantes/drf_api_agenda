faa7ae01bbf8d74cd4df5732e83d98493048ef7e
Yuri Arantes <falaryuriarantes@gmail.com>
2023-08-10 01:08:02 -0300

    Finish tests to schedulings

--- niry_agenda/core/tests.py
+++ niry_agenda/core/tests.py
@@ -7,30 +7,34 @@ from rest_framework.test import APITestCase
 from .models import Scheduling, Store, Schedule, Client
 
 class TestSchedulingList(APITestCase):
-    def set_store(self):
+    def setUp(self) -> None:
         self.store = Store.objects.create(
             social_name="Teste",
             cnpj="89871238000163",
             active = True
         )
-    
-    def set_client_info(self):
+
         self.client_info = Client.objects.create(
             name="Yuri Arantes",
             email="falaryuriarantes@gmail.com",
             phone="33999467305"
         )
 
+        self.schedules = Schedule.objects.create(
+            store=self.store,
+            day=3,
+            first_start_at='09:00:00',
+            first_end_at='12:00:00'
+        )
+
     def test_listagem_vazia(self):
         response = self.client.get("/api/v1/scheduling/")
         data = json.loads(response.content)
         
-        self.assertEqual(data,[])
+        self.assertEqual(response.status_code,200)
+        self.assertListEqual(data,[])
     
-    """
     def test_listagem_de_agendamento_criados(self):
-        self.set_store()
-
         list = [
             {
                 "id":1,
@@ -43,9 +47,7 @@ class TestSchedulingList(APITestCase):
 
         Scheduling.objects.create(
             scheduling_date =datetime(2023,8,10,10,00,00).replace(tzinfo=timezone.utc),
-            name= "Corte Cabelo",
-            email= "faladaoyuriarantes@gmail.com.br",
-            phone= "33999467304",
+            client=self.client_info,
             active= True,
             store=self.store
         )
@@ -55,21 +57,11 @@ class TestSchedulingList(APITestCase):
         data = json.loads(response.content)
 
         self.assertListEqual(data,list)
-    """
+        self.assertEqual(response.status_code,200)
     
     def test_criar_novo_agendamento(self):
-        self.set_store()
-        self.set_client_info()
-
-        Schedule.objects.create(
-            store=self.store,
-            day=3,
-            first_start_at='09:00:00',
-            first_end_at='12:00:00'
-        )
-
         dict = {
-                "scheduling_date":"2023-08-10T10:00:00Z",
+                "scheduling_date":"2023-08-10T09:00:00Z",
                 "store":1,
                 "client":1,
                 "active":"True",
@@ -78,8 +70,6 @@ class TestSchedulingList(APITestCase):
         response = self.client.post(path="/api/v1/scheduling/", data=dict, format='json')
 
         response_data = response.content
-
-        print(response_data)
 
         self.assertEqual(response.status_code,201)
         #self.assertListEqual()
