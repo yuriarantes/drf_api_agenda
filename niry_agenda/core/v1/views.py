4b362c663220738cfc955fa1274c06f640b5e6b3
Yuri Arantes <falaryuriarantes@gmail.com>
2023-08-14 23:57:02 -0300

    Editing views to new defaulting, CBV

--- niry_agenda/core/v1/views.py
+++ niry_agenda/core/v1/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from ..models import Scheduling, Schedule, Store
 from .serializers import SchedulingSerializer
@@ -46,6 +47,45 @@ def scheduling_detail(request, id):
 
         return Response(status=204)
 
+
+class SchedulingList(APIView):
+    def get(self, request):
+        """
+        Search schdulings, return list of scheduling actives
+        """
+        qs = Scheduling.objects.filter(active=True)
+
+        serializer = SchedulingSerializer(qs, many=True)
+
+        return JsonResponse(serializer.data, safe=False)
+
+    def post(self,request):
+        """
+        Create new schdulings, return detail
+        """
+        try:
+            data = request.data
+
+            store = data['store']
+            scheduling_datetime = datetime.strptime(data['scheduling_date'], '%Y-%m-%dT%H:%M:%SZ')
+            scheduling_date = scheduling_datetime.date()
+            scheduling_time = scheduling_datetime.time()
+
+            schedules = SchedulesServices.get_available_times(store,scheduling_date)
+
+            if scheduling_time in schedules:
+                serializer = SchedulingSerializer(data=data)
+
+                if serializer.is_valid():
+                    serializer.save()
+
+                    return JsonResponse(serializer.data, status=201)
+
+                return JsonResponse(serializer.errors, status=400)
+            
+            return JsonResponse({"error":"The specified time is not available."})
+        except Exception as error:
+            return JsonResponse({"error":str(error)}, status=500)
 
 @api_view(http_method_names=['GET','POST'])
 def scheduling_list(request):
