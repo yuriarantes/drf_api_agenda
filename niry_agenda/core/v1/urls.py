d4166c201fec34fa63471cff335929e2b6942a43
Yuri Arantes <falaryuriarantes@gmail.com>
2023-08-14 23:56:14 -0300

    Editing urls to new defaulting, CBV

--- niry_agenda/core/v1/urls.py
+++ niry_agenda/core/v1/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from .views import scheduling_detail, scheduling_list, schedule_list
+from .views import SchedulingList
 
 urlpatterns_schedulings=[
-    path('v1/scheduling/', scheduling_list),
+    path('v1/scheduling/', SchedulingList.as_view()),
     path('v1/scheduling/<int:id>/',scheduling_detail),
 ]
 
