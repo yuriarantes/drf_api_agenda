86defcba5b01a6c080bbd40008964e7e960f9d78
Yuri Arantes <falaryuriarantes@gmail.com>
2023-08-23 02:05:28 -0300

    Editing id some models, i'm use uuid4

--- niry_agenda/core/models.py
+++ niry_agenda/core/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from multiselectfield import MultiSelectField
+
+import uuid
 
 class Client(models.Model):
     name = models.CharField(max_length=240, unique=True)
@@ -27,14 +30,16 @@ class Schedule(models.Model):
         ('6','Sunday'),
     )
 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
     store = models.ForeignKey(Store, related_name="schedules", on_delete=models.CASCADE)
-    day = models.CharField(max_length=20, choices=DAY_CHOICES)
+    day = MultiSelectField(choices=DAY_CHOICES)
     first_start_at = models.TimeField()
     first_end_at = models.TimeField()
     last_start_at = models.TimeField(null=True, blank=True)
     last_end_at = models.TimeField(null=True, blank=True)
 
 class Scheduling(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
     scheduling_date = models.DateTimeField()
     store = models.ForeignKey(Store, related_name="scheduling", on_delete=models.CASCADE, null=False)
     client  = models.ForeignKey(Client, related_name="scheduling", on_delete=models.CASCADE,null=False)
