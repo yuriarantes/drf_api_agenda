a7ef01345be44e2cf5d3e6e487f5303809b238c3
Yuri Arantes <falaryuriarantes@gmail.com>
2023-08-23 02:08:41 -0300

    Editing Schedule url to Class Based Views

--- niry_agenda/core/v1/urls.py
+++ niry_agenda/core/v1/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from .views import schedule_list
+from .views import ScheduleList
 from .views import SchedulingList, SchedulingDetail
 
 urlpatterns_schedulings=[
     path('v1/scheduling/', SchedulingList.as_view()),
-    path('v1/scheduling/<int:id>/',SchedulingDetail.as_view()),
+    path('v1/scheduling/<str:id>/',SchedulingDetail.as_view()),
 ]
 
 urlpatterns_schedules=[
-    path('v1/schedule/',schedule_list)
+    path('v1/schedule/',ScheduleList.as_view())
 ]
